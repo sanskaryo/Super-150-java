1923f9c51e6caee7aa0b24615b655d3717d53d5a
sanskaryo <sanskartodwal@gmail.com>
2024-12-06 19:25:48 +0530

    new questions added

---
+++ arrays/2dArrays/WavePrint.java
@@ -0,0 +1,25 @@
+
+public class WavePrint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] arr = { { 2, 3, 4, 5 }, { 1, 8, 9, 7 }, { 12, 56, 78, 23 } };
+		print(arr);
+	}
+
+	public static void print(int[][] arr) {
+		for (int col = 0; col < arr[0].length; col++) {
+			if (col % 2 == 0) {
+				for (int row = 0; row < arr.length; row++) {
+					System.out.print(arr[row][col] + " ");
+				}
+			} else {
+				for (int row = arr.length - 1; row >= 0; row--) {
+					System.out.print(arr[row][col] + " ");
+				}
+			}
+		}
+
+	}
+
+}
