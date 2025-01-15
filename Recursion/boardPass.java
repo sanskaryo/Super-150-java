c02976d7d70dca6a2ee8bc8197297ea00178f1b1
sanskaryo <sanskartodwal@gmail.com>
2025-01-15 14:32:28 +0530

    more questions on recyursion

---
+++ Recursion/boardPass.java
@@ -0,0 +1,32 @@
+public class boardPass {
+
+    public static void main(String[] args) {
+
+        int n=4;
+
+        Print(n,0, "");
+        
+
+    }
+
+    public static void Print(int n , int curr, String ans){
+
+        if(curr==n){
+            System.out.println(ans);
+            return;
+        }
+        if(curr>n){
+            return;
+        }
+
+        for(int dice =1;dice<=3;dice++){
+            Print(n,curr+dice,ans+dice);
+        }
+
+        // Print(n,curr+1, ans+1);
+        // Print(n, curr+2,ans+2);
+        // Print(n , curr+3, ans+3);
+
+    }
+    
+}
