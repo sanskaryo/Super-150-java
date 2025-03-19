449defc113eb8d02f514d99ff3312fe01df60702
sanskaryo <sanskartodwal@gmail.com>
2025-03-20 01:11:51 +0530

    added linked list

---
+++ Season3/stacks/LinkedLIst/LinkedListClient.java
@@ -0,0 +1,10 @@
+public class LinkedListClient {
+    public static void main(String[] args) {
+        link1 ll = new link1();
+        ll.AddFirst(10);
+        ll.AddFirst(20);
+        ll.AddFirst(30);
+        ll.AddFirst(40);
+        ll.Display();
+    }
+}
